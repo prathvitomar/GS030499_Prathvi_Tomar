a2883fad3695b1724306323889cc940947c607c3
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2025-03-09 13:15:51 +0530

    #5 fixing prod Vercel 404 Issue

--- vite-project/src/pages/LoginPage.tsx
+++ vite-project/src/pages/LoginPage.tsx
@@ -1,39 +1,3 @@
-// import React, { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { login } from "../features/authSlice";
-// import { useNavigate } from "react-router-dom";
-// import Login from "../components/Login/Login";
-
-// const LoginPage: React.FC = () => {
-//   const [formData, setFormData] = useState({ name: "", password: "" });
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-//     setFormData((prev) => ({
-//       ...prev,
-//       [e.target.name]: e.target.value,
-//     }));
-//   }
-
-//   function handleSubmit(e: React.FormEvent) {
-//     e.preventDefault();
-//     dispatch(login(formData));
-//     navigate("/stores");
-//   }
-
-//   return <Login name={formData.name} password={formData.password} handleChange={handleChange} handleSubmit={handleSubmit} />;
-// };
-
-// export default LoginPage;
-
-
-
-
-
-
-
-
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../features/authSlice";
--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
-  ]
+  ],
+  base: '/'
 })
